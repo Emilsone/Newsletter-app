07fbb97da6c8dee9cbccee170469491a011a3022
Your Name Here <lawsonezekiel68@gmail.com>
2023-06-11 16:36:20 +0100

    delete api key

--- src/firestoreInit.js
+++ src/firestoreInit.js
@@ -2,14 +2,8 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: 'AIzaSyBHgT-oUhDc3Ek6KxaiGx_MmsoFlN9CyM4',
-    authDomain: 'newsletter-page-93305.firebaseapp.com',
-    projectId: 'newsletter-page-93305',
-    storageBucket: 'newsletter-page-93305.appspot.com',
-    messagingSenderId: '769518697047',
-    appId: '1:769518697047:web:a02dd0a10fcc37a3340112'
-};
+
+// Paste your firebase config file here 
 
 // Initialize Firebase
 export default firebase.initializeApp(firebaseConfig);
